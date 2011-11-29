b66519c12798db48fae4dbb5fc8eebaba28837d8
Stian Drøbak <sum41rules4@hotmail.com>
2011-11-29 17:56:23 +0100

    Made a first attempt at a javadoc

---
+++ Backend/Broadcaster.java
@@ -0,0 +1,7 @@
+public class Broadcaster{
+	/**
+	 * Class for broadcasting
+	 * 
+	 * Used to communicate to scanners that it is available
+	 */
+}
---
+++ Backend/Scanner.java
@@ -0,0 +1,7 @@
+public class Scanner{
+	/**
+ 	* Scan for broadcast
+ 	*
+ 	* Used to look for systems that broadcast that they are availabel
+ 	*/
+}
